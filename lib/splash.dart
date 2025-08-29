563645656dd53fb5ffe5ff7d96be5c7e54b14e90
Pouria <pouriasarkhosh@yahoo.com>
2025-08-29 15:13:22 +0330

    add splashScreen

---
+++ lib/splash.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog_club/home.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SplashScreen extends StatefulWidget{
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+  
+}
+
+
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3)).then((value) {
+      // ignore: use_build_context_synchronously
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>HomeScreen()));
+    },); 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset('assets/img/background/splash.png',fit: BoxFit.cover,),
+
+          Center(child: SvgPicture.asset('assets/img/icons/LOGO.svg'))
+        ],
+      ) ,
+    );
+  }
+}
